62d9580c2e4186886fdd2b488f63b12d662c3908
danielogen <danielogen@gmail.com>
2020-09-30 17:11:26 +0300

    more updates

--- devops/microservice-1/app.py
+++ devops/microservice-1/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World From Python Flask + Demo!"
+    return "Hello World From Python Flask + Demo x2!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
