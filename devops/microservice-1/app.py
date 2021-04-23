85e91ba79431cd37a337acb219e5ad1b1b56bbca
danielogen <danielogen@gmail.com>
2021-04-23 13:26:26 +0300

    update

--- devops/microservice-1/app.py
+++ devops/microservice-1/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Health check passed!-Hello world"
+    return "Health check passed!-Hello world test"
 
 
 if __name__ == '__main__':
