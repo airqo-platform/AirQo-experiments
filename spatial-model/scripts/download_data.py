f445ece7dba857e86ba693b1fa1e93a8b864e005
lilly-muyama <muyamalillian@gmail.com>
2021-05-27 10:44:53 +0300

    refactoring spatial model code

---
+++ spatial-model/scripts/download_data.py
@@ -0,0 +1,104 @@
+import pandas as pd 
+import numpy as np
+from google.cloud import bigquery
+import pickle
+from datetime import datetime, timedelta
+import sys
+
+
+def get_location_data(path, columns):
+    """Gets details of locations considered
+
+    Parameters
+    ----------
+    path : str
+        The file location of the spreadsheet
+    columns: list
+        The names of the columns to be considered
+
+    Returns
+    -------
+    df : DataFrame
+        a pandas dataframe consisting of the locations' data in the spreadsheet
+    """
+    df = pd.read_csv(path, usecols=columns)
+    return df
+
+def get_loc(channel_id):
+    """Gets details of locations considered
+
+    Parameters
+    ----------
+    channel_id : str
+        The channel ID of the device
+
+    Returns
+    -------
+    df : DataFrame
+        a pandas dataframe consisting of the device's geographical coordinates and channel ID
+    """
+
+    sql = """
+    SELECT channel_id, longitude, latitude 
+    FROM `airqo-250220.thingspeak.channel`
+    WHERE channel_id={}
+    """.format(channel_id)
+
+    df = client.query(sql).to_dataframe()
+    return df
+
+def preprocessing(df):
+    """Preprocesses data for a particular device
+
+    Parameters
+    ----------
+    df : DataFrame
+        a panda dataframe that has a partuclar device's data
+
+    Returns
+    -------
+    df : DataFrame
+        a pandas dataframe with the preprocessed data of the device
+    """
+    df = df.sort_values(by='created_at',ascending=False)
+    df = df.set_index('created_at')
+    hourly_df = df.resample('H').mean()
+    hourly_df.dropna(inplace=True)
+    hourly_df= hourly_df.reset_index()
+    return hourly_df
+
+def get_entries_since(channel_id, start_date='2020-09-17 00:00:00', end_date='2020-09-23 23:59:59'):
+    """Gets a device's data between a specified period
+
+    Parameters
+    ----------
+    channel_id : str
+        The channel ID of a device
+    start_date: str
+        The start date of the peiod of interest
+    end_date:str
+        The end date of the period of interest
+
+    Returns
+    -------
+    df : DataFrame
+        a pandas dataframe consisting of a device's data within a specified period
+    """
+    client = bigquery.Client.from_service_account_json("C:/Users/User/AirQo-d982995f6dd8.json")
+
+    sql = """
+    SELECT created_at, channel_id, pm2_5 
+    FROM `airqo-250220.thingspeak.clean_feeds_pms` 
+    WHERE channel_id={} 
+    AND created_at between '{}' AND '{}'
+    """.format(channel_id, start_date, end_date)
+
+    df = client.query(sql).to_dataframe() 
+    return df  
+
+
+if __name__=='__main__':
+    locations_df = get_location_data('data/raw/channels.csv', ['location', 'id', 'lat', 'long'])
+    channels = list(locations_df.id)
+    print (channels)
+    
---
+++ spatial-model/scripts/predict.py
@@ -0,0 +1,154 @@
+import pickle
+import gpflow
+import numpy as np
+from gpflow import set_trainable
+from math import sqrt
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas import Timestamp
+from datetime import timedelta
+from statistics import mean
+
+def get_location_data(path, columns):
+    """Gets details of locations considered
+
+    Parameters
+    ----------
+    path : str
+        The file location of the spreadsheet
+    columns: list
+        The names of the columns to be considered
+
+    Returns
+    -------
+    df : DataFrame
+        a pandas dataframe consisting of the locations' data in the spreadsheet
+    """
+    df = pd.read_csv(path, usecols=columns)
+    return df
+
+def get_data(path):
+    """Loads data from saved file
+
+    Parameters
+    ----------
+    path : str
+        The file location of the pickle file
+
+    Returns
+    -------
+    X : Array
+        a numpy array consisting of the features data
+    Y : Array
+        a numpy array consisting of the target data
+    """
+    d = pickle.load(open(path,'rb'))
+    X = d['X']
+    Y = d['Y']
+    return X, Y
+
+def train_model(Xtraining, Ytraining):
+    """Develops and trains a model
+
+    Parameters
+    ----------
+    Xtraining : Array
+        The X training data
+    Ytraining : Array
+        The target training data
+
+    Returns
+    -------
+    m : model
+        a GPR model trained on the training data
+    """
+    
+    #defining kernel
+    k = gpflow.kernels.RBF(lengthscales=[0.08, 0.08, 1.], variance=625) + gpflow.kernels.Bias()
+    #defining model
+    m = gpflow.models.GPR(data=(Xtraining, Ytraining), kernel=k, mean_function=None)
+    m.likelihood.variance.assign(400)
+    set_trainable(m.kernel.kernels[0].lengthscales, False)
+    set_trainable(m.kernel.kernels[0].variance, False)
+    set_trainable(m.likelihood.variance, False)
+    #optimization
+    opt = gpflow.optimizers.Scipy()
+    def objective_closure():
+             return - m.log_marginal_likelihood()
+
+    opt_logs = opt.minimize(objective_closure, m.trainable_variables, options=dict(maxiter=100))
+    return m
+
+def make_predictions(m, Xtest, Ytest):
+    """Gets details of locations considered
+
+    Parameters
+    ----------
+    m : GPR model
+        The trained model
+    Xtest : array
+        The X test data
+    Ytest : array
+        The Y test data
+
+    Returns
+    -------
+    mean : array
+        a numpy array of the predicted values
+    var : array
+        a numpy array of the variances of the predictions
+    rmse : int
+        the error between the predicted and actual values
+    """
+    mean, var = m.predict_f(Xtest)
+    rmse = sqrt(mean_squared_error(Ytest, mean.numpy()))
+    return mean, var, rmse
+
+def cross_validation(X, Y, long, lat):
+    """Gets details of locations considered
+
+    Parameters
+    ----------
+    X : str
+        The features data
+    Y : list
+        The target data
+    long : float
+        The longitude coordinate of the device's location
+    lat : float
+        The latitude coordinate of the device's location
+
+    Returns
+    -------
+    mean : array
+        a numpy array of the predicted values
+    var : Array
+        a numpy array of the variances of the predictions
+    Xtest : array
+        a numpy array of the X test data
+    Ytest : array
+        a numpy array of the target test data
+    rmse : int
+        the error between the actual and predicted values
+    """
+    location_indices = np.where(np.logical_and(X[:,0]==long, X[:,1]==lat))
+    
+    Xset = X[np.logical_not(np.logical_and(X[:,0]==long, X[:,1]==lat))]
+    Yset = np.delete(Y, slice(location_indices[0][0],location_indices[0][-1]+1), axis=0)
+        
+    Xtraining = Xset[::2,:]
+    Ytraining = Yset[::2,:]
+    
+    Xtest = X[np.logical_and(X[:,0]==long, X[:,1]==lat)]
+    Ytest = Y[location_indices[0][0]:location_indices[0][-1]+1]
+
+    m = train_model(Xtraining, Ytraining)
+    mean, var, rmse = make_predictions(m, Xtest, Ytest) 
+    return mean.numpy(), var.numpy(), Xtest, Ytest, round(rmse, 2)
+
+
+if __name__=='__main__':
+    locations_df = get_location_data('data/raw/channels.csv', ['location', 'lat', 'long'])
+    X, Y = get_data('data/raw/data.p')
+    print(locations_df.shape[0], X.shape, Y.shape)
